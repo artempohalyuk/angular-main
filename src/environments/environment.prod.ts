55f04b3fa28d30f15b3c6dda7a753a620e0744ae
Artem Pokhaliuk <artempohalyuk@gmail.com>
2023-08-02 08:27:45 +0300

    change auth url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment: IEnvironment = {
   production: true,
   apiEndpoint: 'https://server-microfrontend.vercel.app/api',
-  authUrl: 'https://react-auth-microfrontend.vercel.app'
+  authUrl: 'https://react.top-web.in.ua/'
 };
